0bbbdb3bdee8102a43856d43fe978df2bf07a328
Gregory Kaiser <ghkaiser112@gmail.com>
2020-11-24 22:01:18 -0500

    split obstacle to new file

---
+++ obstacle.py
@@ -0,0 +1,21 @@
+class obstacle():
+
+    def __init__(self, x, y, width, height, env_type):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.speedx = 0 #move with background
+        self.speedy = 0 #always zero
+        self.type = env_type
+
+    def draw(self, win):
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
+        pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+        win.blit(image, (self.x, self.y))
+
+    def collide(self, rect): #copied from spike
+        if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
+            if rect[1] < self.hitbox[3]:
+                return True
+        return False
