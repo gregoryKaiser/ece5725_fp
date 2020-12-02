ae25cd8e748af50bf4581c78b1380a7fc7601442
Gregory Kaiser <ghkaiser112@gmail.com>
2020-12-02 18:48:57 -0500

    added os tft stuff

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
+#TFT stuff
+os.putenv('SDL_VIDEODRIVER','fbcon')
+os.putenv('SDL_FBDEV','/dev/fb1')#might have to change to fb1
+os.putenv('SDL_MOUSEDRV','TSLIB') #Track mouse clicks on piTFT
+os.putenv('SDL_MOUSEDEV','/dev/input/touchscreen')
 
 GPIO.setmode(GPIO.BCM)
 
